a0502b32c90db9072b14aaa18d6f2d5ac943ba12
박정환 <ggssgg1028@naver.com>
2022-05-15 17:23:24 +0900

    baekjoon 1436 end

--- baekjoon1436.c
+++ baekjoon1436.c
@@ -5,18 +5,18 @@ int main(void) {
 
 	int num;
 	int count = 0;
+	int flag = 0;
 
 	scanf("%d", &num);
 
 	int digit[10] = { 0, };
 
-	for (int i = 0; i < 1000000; i++) {
-
-		int a, b, c;
+	for (int i = 0; i < 10000000; i++) {
+		flag = 0;
+		int a, b;
 		a = 10;
 		b = 1;
-		c = 100;
-		
+
 		for (int j = 0; j < 10; j++) {
 			digit[j] = (i % a) / b;
 			a *= 10;
@@ -28,23 +28,23 @@ int main(void) {
 			if (digit[j] == 6 &&
 				digit[j + 1] == 6 &&
 				digit[j + 2] == 6) {
-				count++;
+				flag++;
+			}
+		}
+		if (flag >= 1) {
+			int k;
+			count++;
 
-				if (count == num) {
+			for (k = 9; k >= 0; k--) {
+				if (digit[k] == 0) continue;
+				else break;
+			}
 
-					int k;
-
-					for (k = 4; k >= 0; k--) {
-						if (digit[k] == 0) continue;
-						else break;
-					}
-
-					for (; k >= 0; k--) {
-						printf("%d", digit[k]);
-					}
-					printf("\ncount : %d", count);
-					break;
+			if (count == num) {
+				for (; k >= 0; k--) {
+					printf("%d", digit[k]);
 				}
+				printf("\n");
 			}
 		}
 	}
